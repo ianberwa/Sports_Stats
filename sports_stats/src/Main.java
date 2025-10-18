e7ab5a72794e0bbcda3e6e6b83be88f88fb1668e
Berwa Ian Ange <ian.berwa@strathmore.edu>
2025-10-18 16:40:25 +0300

    Adding the java code for data analysis

---
+++ sports_stats/src/Main.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/";
+        String username = "root";
+        String password = "";
+        Connection con= DriverManager.getConnection(url,username,password);
+
+
+    }
+}
